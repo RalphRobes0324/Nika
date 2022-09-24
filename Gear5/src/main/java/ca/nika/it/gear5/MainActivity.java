0ffa14d7d777f7c386ef45794e710d722dcebf3f
ElijahTanimowo3560 <n01433560@humbermail.ca>
2022-09-23 20:05:46 -0400

    Bottom Nav now functions and made fragments have unique background colours, Elijah Tanimowo N01433560

--- Gear5/src/main/java/ca/nika/it/gear5/MainActivity.java
+++ Gear5/src/main/java/ca/nika/it/gear5/MainActivity.java
@@ -1,14 +1,46 @@
 package ca.nika.it.gear5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import ca.nika.it.gear5.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new PlayFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.play:
+                    replaceFragment(new PlayFragment());
+                    break;
+                case R.id.score:
+                    replaceFragment(new ScoreFragment());
+                    break;
+                case R.id.login:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
