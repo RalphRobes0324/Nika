435be136374cd402a45b9ef7dcbca5d9c46c89d7
PradeepSingh0573 <pradeep.heer93@gmail.com>
2022-09-25 13:47:09 -0400

    Added the exit on backpress dialog, with updated icon.

--- Gear5/src/main/java/ca/nika/it/gear5/MainActivity.java
+++ Gear5/src/main/java/ca/nika/it/gear5/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import ca.nika.it.gear5.databinding.ActivityMainBinding;
@@ -44,5 +46,25 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.commit();
+    }
+    @Override
+    public void onBackPressed(){
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.Gear5)
+                .setMessage(R.string.ExitApp)
+                .setIcon(R.drawable.devilfruit)
+                .setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+
     }
 }
