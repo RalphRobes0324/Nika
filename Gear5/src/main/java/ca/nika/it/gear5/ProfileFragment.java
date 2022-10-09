a95c17dc5424b7cb6ee8a801362466d1318317cd
PradeepSingh0573 <pradeep.heer93@gmail.com>
2022-10-09 19:24:19 -0400

    Added runtime permission to allow app to access gallery

--- Gear5/src/main/java/ca/nika/it/gear5/ProfileFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/ProfileFragment.java
@@ -2,19 +2,32 @@
 // CENG-322-0NB Ralph Robes n01410324, Elijah Tanimowo n01433560
 package ca.nika.it.gear5;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
+    ImageView mImageVIew;
+    Button mChooseBtn;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,66 @@ public class ProfileFragment extends Fragment {
 
     }
 
+    private void pickImageFromGallery() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(getActivity(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            mImageVIew.setImageURI(data.getData());
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mImageVIew = view.findViewById(R.id.image_view);
+        mChooseBtn = view.findViewById(R.id.choose_image_btn);
+        mChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED) {
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+
+                        pickImageFromGallery();
+
+                    }
+
+                } else {
+                    pickImageFromGallery();
+                }
+            }
+        });
+
+
+        return view;
     }
 }
