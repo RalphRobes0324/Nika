6b71fb2686348fb3c3c04eb8831aa09514ec5527
RalphRobes0324 <ralphrobes123@gmail.com>
2022-10-09 11:54:46 -0400

    Change splash setting and change some parts frag reg

--- Gear5/src/main/java/ca/nika/it/gear5/SplashActivity.java
+++ Gear5/src/main/java/ca/nika/it/gear5/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class)); //change to login activity when done
                 finish();
             }
         }, 2000);
