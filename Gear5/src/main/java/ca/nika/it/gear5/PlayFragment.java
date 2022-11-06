3dcc019ba13337ad053fbcc7dc21e270157a1017
FranciscoSantos3860 <n01423860@humber.ca>
2022-11-06 10:33:46 -0500

    Added Joystick Implementation

--- Gear5/src/main/java/ca/nika/it/gear5/PlayFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/PlayFragment.java
@@ -11,14 +11,23 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class PlayFragment extends Fragment {
     Button btnA;
     Button btnB;
     private View v;
+    RelativeLayout layout_joystick;
+    ImageView image_joystick, image_border;
+    TextView textView1, textView2, textView3, textView4, textView5;
+
+    JoyStickClass js;
 
     public PlayFragment() {
         // Required empty public constructor
@@ -55,24 +64,66 @@ public class PlayFragment extends Fragment {
         });
 
         loadGameSetting();
+
+        textView1 = (TextView) v.findViewById(R.id.textView1);
+        textView2 = (TextView) v.findViewById(R.id.textView2);
+        textView3 = (TextView) v.findViewById(R.id.textView3);
+        textView4 = (TextView) v.findViewById(R.id.textView4);
+        textView5 = (TextView) v.findViewById(R.id.textView5);
+
+        layout_joystick = (RelativeLayout) v.findViewById(R.id.layout_joystick);
+
+        js = new JoyStickClass(getActivity().getApplicationContext(), layout_joystick, R.drawable.red_circle);
+        js.setStickSize(270, 270);
+        js.setLayoutSize(500, 500);
+//        js.setLayoutAlpha(150);
+//        js.setStickAlpha(100);
+        js.setOffset(90);
+        js.setMinimumDistance(50);
+
+        layout_joystick.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                js.drawStick(arg1);
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN
+                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+                    textView1.setText("X : " + String.valueOf(js.getX()));
+                    textView2.setText("Y : " + String.valueOf(js.getY()));
+                    textView3.setText("Angle : " + String.valueOf(js.getAngle()));
+                    textView4.setText("Distance : " + String.valueOf(js.getDistance()));
+
+                    int direction = js.get8Direction();
+                    if(direction == JoyStickClass.STICK_UP) {
+                        textView5.setText("Direction : Up");
+                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
+                        textView5.setText("Direction : Up Right");
+                    } else if(direction == JoyStickClass.STICK_RIGHT) {
+                        textView5.setText("Direction : Right");
+                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
+                        textView5.setText("Direction : Down Right");
+                    } else if(direction == JoyStickClass.STICK_DOWN) {
+                        textView5.setText("Direction : Down");
+                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
+                        textView5.setText("Direction : Down Left");
+                    } else if(direction == JoyStickClass.STICK_LEFT) {
+                        textView5.setText("Direction : Left");
+                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
+                        textView5.setText("Direction : Up Left");
+                    } else if(direction == JoyStickClass.STICK_NONE) {
+                        textView5.setText("Direction : Center");
+                    }
+                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
+                    textView1.setText("X :");
+                    textView2.setText("Y :");
+                    textView3.setText("Angle :");
+                    textView4.setText("Distance :");
+                    textView5.setText("Direction :");
+                }
+                return true;
+            }
+        });
         return v;
     }
 
-    public void doSave() {
-        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(getString(R.string.SettingsPref), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        getParentFragmentManager().setFragmentResultListener("getBundlePass", this, new FragmentResultListener() {
-            @Override
-            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
-                int num = result.getInt("bundlePass");
-                editor.putInt("Index", num);
-
-            }
-        });
-        editor.apply();
-
-    }
 
     private void loadGameSetting()  {
         SharedPreferences sharedPreferences= this.getActivity().getSharedPreferences(getString(R.string.SettingsPref), Context.MODE_PRIVATE);
