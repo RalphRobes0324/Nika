ca07279c0bb81b35790f256af1089430a0832033
RalphRobes0324 <ralphrobes123@gmail.com>
2022-11-06 10:09:24 -0500

    Added Google sign in but fails open google sign in itself. For now its being fixed

--- Gear5/src/main/java/ca/nika/it/gear5/LoginSetup/LoginFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/LoginSetup/LoginFragment.java
@@ -11,11 +11,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Handler;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,8 +29,19 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -45,6 +58,11 @@ public class LoginFragment extends Fragment {
     private CheckBox remember;
     private EditText usernameInput, passwordInput;
     private LinearLayout googleBtn;
+
+    private static final int REQ_ONE_TAP = 1;
+    GoogleSignInClient mGoogleSignInClient;
+    private boolean showOneTapUI = true;
+
 
 
     DatabaseReference databaseReference;
@@ -64,6 +82,9 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+
+
+
         registerBtn = (Button) view.findViewById(R.id.nika_btn_login_reg);
         forgortpwdBtn = (Button) view.findViewById(R.id.nika_btn_forgotPwd_loginFrag);
         loginBtn = (Button) view.findViewById(R.id.nika_btn_login_login);
@@ -77,7 +98,13 @@ public class LoginFragment extends Fragment {
         googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getActivity().getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                //google
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+                mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+                signIn();
             }
         });
 
@@ -126,6 +153,45 @@ public class LoginFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQ_ONE_TAP);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_ONE_TAP){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+
+            }catch (ApiException e){
+                Toast.makeText(getActivity().getApplicationContext(), "Failed Sign-in Google", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }
+                        else{
+                            Toast.makeText(getActivity().getApplicationContext(),
+                                    "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void validateUserAndPwd(String username, String password) {
