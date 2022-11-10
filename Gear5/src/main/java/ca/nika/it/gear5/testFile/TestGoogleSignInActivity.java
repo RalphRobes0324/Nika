654429f801bfb32c408edea321a6200bbdf39619
RalphRobes0324 <ralphrobes123@gmail.com>
2022-11-10 08:38:43 -0500

    Test Files

--- Gear5/src/main/java/ca/nika/it/gear5/testFile/TestGoogleSignInActivity.java
+++ Gear5/src/main/java/ca/nika/it/gear5/testFile/TestGoogleSignInActivity.java
@@ -1,5 +1,10 @@
 package ca.nika.it.gear5.testFile;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +18,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import ca.nika.it.gear5.MainActivity;
 import ca.nika.it.gear5.R;
@@ -21,6 +32,32 @@ import ca.nika.it.gear5.R;
 public class TestGoogleSignInActivity extends AppCompatActivity {
     GoogleSignInOptions googleSignInOptions;
     GoogleSignInClient googleSignInClient;
+    FirebaseAuth mAuth;
+    ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult activityResult) {
+                            int result = activityResult.getResultCode();
+                            Intent data = activityResult.getData();
+                            if (result == RESULT_OK){
+                                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                                try {
+                                    task.getResult(ApiException.class);
+                                    enterMainActivity();
+
+                                } catch (ApiException e) {
+                                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Failed to Load", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +65,7 @@ public class TestGoogleSignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_google_sign_in);
 
         googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
@@ -40,6 +78,7 @@ public class TestGoogleSignInActivity extends AppCompatActivity {
     private void signIn() {
         Intent intent = googleSignInClient.getSignInIntent();
         startActivityForResult(intent, 1000);
+        //activityResultLauncher.launch(intent);
     }
 
     @Override
@@ -48,14 +87,30 @@ public class TestGoogleSignInActivity extends AppCompatActivity {
         if (requestCode == 1000){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
-                task.getResult(ApiException.class);
-                enterMainActivity();
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
 
             } catch (ApiException e) {
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
 
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            enterMainActivity();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Failed to Auth", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void enterMainActivity() {
