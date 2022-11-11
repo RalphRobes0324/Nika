046d355b6a043ddbfd454dc6b4ef44828acb9334
RalphRobes0324 <ralphrobes123@gmail.com>
2022-11-11 11:42:51 -0500

    putting our names in my files

--- Gear5/src/main/java/ca/nika/it/gear5/LoginSetup/UserPhoneValidationFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/LoginSetup/UserPhoneValidationFragment.java
@@ -1,5 +1,6 @@
 package ca.nika.it.gear5.LoginSetup;
-
+// CENG-322-0NC Francisco Santos n01423860, Pradeep Singh n00975892
+// CENG-322-0NB Ralph Robes n01410324, Elijah Tanimowo n01433560
 import android.content.Context;
 import android.os.Bundle;
 
