12f1acf4d1d146737b719b03a194b65dc17f96c0
ElijahTanimowo3560 <n01433560@humbermail.ca>
2022-11-13 15:29:01 -0500

    updated payment dialog to include card number, expiration date and cvv Elijah Tanimowo N01433560

--- Gear5/src/main/java/ca/nika/it/gear5/BalanceFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/BalanceFragment.java
@@ -9,16 +9,18 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class BalanceFragment extends Fragment{
 
     PreferenceManager preferenceManager;
+    EditText editTextNumber,editTextEXP,editTextCVV;
+    AlertDialog dialog;
 
     public BalanceFragment() {
         // Required empty public constructor
@@ -38,7 +40,6 @@ public class BalanceFragment extends Fragment{
         if (sharedPreferences != null) {
 
             String getUserId = sharedPreferences.getString(getString(R.string.userProfile), getString(R.string.blank));
-
 
         }
     }
@@ -61,7 +62,7 @@ public class BalanceFragment extends Fragment{
                 AlertDialog dialog = new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.gear500title)
                         .setMessage(R.string.gear500msg)
-                        .setPositiveButton(R.string.confirm,new DialogInterface.OnClickListener(){
+                        .setPositiveButton(R.string.Continue,new DialogInterface.OnClickListener(){
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 openDialog();
@@ -78,7 +79,12 @@ public class BalanceFragment extends Fragment{
                 AlertDialog dialog = new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.gear1000title)
                         .setMessage(R.string.gear1100msg)
-                        .setPositiveButton(R.string.confirm,null)
+                        .setPositiveButton(R.string.Continue,new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                openDialog();
+                            }
+                        })
                         .setNegativeButton(R.string.cancel,null)
                         .show();
             }
@@ -90,7 +96,12 @@ public class BalanceFragment extends Fragment{
                 AlertDialog dialog = new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.gear2200title)
                         .setMessage(R.string.gear2200msg)
-                        .setPositiveButton(R.string.confirm,null)
+                        .setPositiveButton(R.string.Continue,new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                openDialog();
+                            }
+                        })
                         .setNegativeButton(R.string.cancel,null)
                         .show();
             }
@@ -102,7 +113,12 @@ public class BalanceFragment extends Fragment{
                 AlertDialog dialog = new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.gear5700title)
                         .setMessage(R.string.gear5700msg)
-                        .setPositiveButton(R.string.confirm,null)
+                        .setPositiveButton(R.string.Continue,new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                openDialog();
+                            }
+                        })
                         .setNegativeButton(R.string.cancel,null)
                         .show();
             }
@@ -114,7 +130,12 @@ public class BalanceFragment extends Fragment{
                 AlertDialog dialog = new AlertDialog.Builder(getActivity())
                         .setTitle(R.string.gear11600title)
                         .setMessage(R.string.gear11600msg)
-                        .setPositiveButton(R.string.confirm,null)
+                        .setPositiveButton(R.string.Continue,new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                openDialog();
+                            }
+                        })
                         .setNegativeButton(R.string.cancel,null)
                         .show();
             }
@@ -124,8 +145,58 @@ public class BalanceFragment extends Fragment{
     }
 
     public void openDialog() {
-        PaymentDialog paymentDialog = new PaymentDialog();
-        paymentDialog.show(getFragmentManager(),"payment dialog");
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.payment);
+
+        View view = getLayoutInflater().inflate(R.layout.layout_dialog,null);
+
+        editTextNumber = view.findViewById(R.id.edit_card_num);
+        editTextEXP = view.findViewById(R.id.edit_card_exp);
+        editTextCVV = view.findViewById(R.id.edit_card_cvv);
+
+        builder.setView(view);
+
+        dialog = builder.create();
+
+        dialog.show();
     }
+
+    /*(num.isEmpty()){
+            editTextNumber.setError("Card Number is Required");
+            editTextNumber.requestFocus();
+            button.setEnabled(false);
+        }
+
+        if(num.length()!=16){
+            editTextNumber.setError("Card Number length should be 16");
+            editTextNumber.requestFocus();
+            button.setEnabled(false);
+        }
+
+        if(exp.isEmpty()){
+            editTextEXP.setError("Expiration Date is Required");
+            editTextEXP.requestFocus();
+            button.setEnabled(false);
+        }
+
+        if(exp.length()!=4){
+            editTextNumber.setError("Expiration Date length should be 4");
+            editTextNumber.requestFocus();
+            button.setEnabled(false);
+        }
+
+        if(cvv.isEmpty()){
+            editTextEXP.setError("CVV is Required");
+            editTextEXP.requestFocus();
+            button.setEnabled(false);
+        }
+
+        if(cvv.length()!=3){
+            editTextCVV.setError("CVV length should be 3");
+            editTextCVV.requestFocus();
+            button.setEnabled(false);
+        }*/
 
 }
