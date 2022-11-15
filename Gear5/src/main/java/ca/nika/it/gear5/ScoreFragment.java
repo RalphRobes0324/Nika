5d78b312216d9825a44f9f19d5bea7aa9c604173
FranciscoSantos3860 <n01423860@humber.ca>
2022-11-14 20:22:54 -0500

    added aSyncTask use in score fragment to display image

--- Gear5/src/main/java/ca/nika/it/gear5/ScoreFragment.java
+++ Gear5/src/main/java/ca/nika/it/gear5/ScoreFragment.java
@@ -2,37 +2,40 @@
 // CENG-322-0NB Ralph Robes n01410324, Elijah Tanimowo n01433560
 package ca.nika.it.gear5;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
-
-import ca.nika.it.gear5.LoginSetup.UserClass;
 
 public class ScoreFragment extends Fragment {
 
     private TextView nikaScore1TextView, nikaScore2TextView, nikaScore3TextView;
 
-    String nikaScoreDisplay1;
     ArrayList<String> arrayListOfUsers = new ArrayList<String>();
     ArrayList<Integer> arrayListOfUserScores = new ArrayList<Integer>();
+    ProgressDialog progress;
+    ImageView nikaSyncTaskImage;
 
     public ScoreFragment() {
 
@@ -44,6 +47,40 @@ public class ScoreFragment extends Fragment {
 
     }
 
+    private class LoadImage extends AsyncTask<Void,Void,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            Bitmap bitmap=null;
+
+            try {
+                bitmap= BitmapFactory.decodeStream((InputStream)new URL(getString(R.string.onlineLink)).getContent());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //show progress dialog while image is loading
+            progress=new ProgressDialog(getActivity());
+            progress.setMessage(getString(R.string.loadingImage));
+            progress.show();
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if(bitmap!=null) {
+                nikaSyncTaskImage.setImageBitmap(bitmap);
+                progress.dismiss();
+            } else {
+                progress.dismiss();
+                Toast.makeText(getActivity(), getString(R.string.errorOccured),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -52,6 +89,7 @@ public class ScoreFragment extends Fragment {
         nikaScore1TextView = (TextView) view.findViewById(R.id.nikaScore1);
         nikaScore2TextView = (TextView) view.findViewById(R.id.nikaScore2);
         nikaScore3TextView = (TextView) view.findViewById(R.id.nikaScore3);
+        nikaSyncTaskImage = (ImageView) view.findViewById(R.id.nika_aSync);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(getString(R.string.childRef_reg_regFrag));
@@ -65,13 +103,9 @@ public class ScoreFragment extends Fragment {
                     arrayListOfUsers.add(username);
                 }
 
-                nikaScore1TextView.setText(arrayListOfUsers.get(0) + arrayListOfUserScores.get(0).intValue());
-                nikaScore2TextView.setText(arrayListOfUsers.get(1)  + arrayListOfUserScores.get(1).intValue());
-                nikaScore3TextView.setText(arrayListOfUsers.get(2)  + arrayListOfUserScores.get(2).intValue());
-
-                int end = arrayListOfUsers.size();
-                int start = 0;
-
+                nikaScore1TextView.setText(getString(R.string.nameDisplay) + arrayListOfUsers.get(0) + getString(R.string.topScore) + arrayListOfUserScores.get(0).intValue());
+                nikaScore2TextView.setText(getString(R.string.nameDisplay) + arrayListOfUsers.get(1) + getString(R.string.topScore) + arrayListOfUserScores.get(1).intValue());
+                nikaScore3TextView.setText(getString(R.string.nameDisplay) + arrayListOfUsers.get(2) + getString(R.string.topScore) + arrayListOfUserScores.get(2).intValue());
 
 
             }
@@ -84,6 +118,7 @@ public class ScoreFragment extends Fragment {
 
 
 
+        new LoadImage().execute();
 
         return view;
 
