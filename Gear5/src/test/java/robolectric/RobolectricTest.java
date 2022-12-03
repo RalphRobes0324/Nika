e44ac36fe68f94c37d1283870e889a11305fda48
ElijahTanimowo3560 <n01433560@humbermail.ca>
2022-12-03 03:35:21 -0500

    created several Robolectric Tests Elijah Tanimowo N01433560

--- Gear5/src/test/java/robolectric/RobolectricTest.java
+++ Gear5/src/test/java/robolectric/RobolectricTest.java
@@ -9,13 +9,22 @@ import org.robolectric.RobolectricTestRunner;
 
 import static android.os.Build.VERSION_CODES.S;
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
 
 import android.content.pm.ActivityInfo;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import org.robolectric.android.controller.ActivityController;
+
+import ca.nika.it.gear5.LoginSetup.LoginActivity;
 import ca.nika.it.gear5.MainActivity;
 import ca.nika.it.gear5.R;
+import ca.nika.it.gear5.SignInFile.GoogleSignInActivity;
+import ca.nika.it.gear5.SplashActivity;
 
 @RunWith(RobolectricTestRunner.class)
 @Implements(value = org.robolectric.shadows.ShadowBackdropFrameRenderer.class, minSdk = S, isInAndroidSdk = false)
@@ -32,13 +41,89 @@ public class RobolectricTest {
         }
     }
 
-    //verify that landscape works
+    //verify that landscape works for main
     @Test
-    public void checkLandscape() {
+    public void checkLandscapeMain() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity mainActivity = controller.get();
+            mainActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+    }
+
+    //verify that landscape works for login
+    @Test
+    public void checkLandscapeLogin() {
+        try (ActivityController<LoginActivity> controller = Robolectric.buildActivity(LoginActivity.class)) {
+            controller.setup();
+            LoginActivity loginActivity = controller.get();
+            loginActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+    }
+
+    //verify frame layout exist
+    @Test
+    public void frameLayoutCheck() {
         try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
             controller.setup();
             MainActivity activity = controller.get();
-            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            FrameLayout checker = (FrameLayout) activity.findViewById(R.id.nikaFrameLayout);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
+        }
+    }
+
+    //verify bottom nav exist
+    @Test
+    public void bottomNavCheck() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity activity = controller.get();
+            BottomNavigationView checker = (BottomNavigationView) activity.findViewById(R.id.nikaBottomNavigationView);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
+        }
+    }
+
+    //verify bottom nav item play exist
+    @Test
+    public void bottomNavPlayCheck() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity activity = controller.get();
+            BottomNavigationItemView checker = (BottomNavigationItemView) activity.findViewById(R.id.play);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
+        }
+    }
+
+    //verify bottom nav item score exist
+    @Test
+    public void bottomNavScoreCheck() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity activity = controller.get();
+            BottomNavigationItemView checker = (BottomNavigationItemView) activity.findViewById(R.id.score);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
+        }
+    }
+
+    //verify bottom nav item profile exist
+    @Test
+    public void bottomNavProfileCheck() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity activity = controller.get();
+            BottomNavigationItemView checker = (BottomNavigationItemView) activity.findViewById(R.id.profile);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
+        }
+    }
+
+    //verify bottom nav item profile exist (test should fail)
+    @Test
+    public void bottomNavInvalidCheck() {
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            MainActivity activity = controller.get();
+            BottomNavigationItemView checker = (BottomNavigationItemView) activity.findViewById(R.id.submit_review);
+            assertNotNull(activity.getString(R.string.image_not_shown), checker);
         }
     }
 }
