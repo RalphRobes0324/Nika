5aa1ce7d78518e5db9447cdad6c781675c056e74
PradeepSingh0573 <pradeep.heer93@gmail.com>
2022-12-03 02:16:30 -0500

    Junit4 testing, updated 4 test methods

--- Gear5/src/test/java/junit4/junit4Test.java
+++ Gear5/src/test/java/junit4/junit4Test.java
@@ -1,17 +1,70 @@
 package junit4;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class junit4Test {
 
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Test
-    public void doSave() {
+    public void isEmptyTest() {
+        assertTrue(isEmpty(null));
+        assertTrue(isEmpty(""));
+        assertFalse(isEmpty(" "));
+        assertFalse(isEmpty("nika"));
+        assertFalse(isEmpty("  nika  "));
+    }
 
+    @Test
+    public void isBlankTest() {
+        assertTrue(isBlank(null));
+        assertTrue(isBlank(""));
+        assertTrue(isBlank(" "));
+        assertFalse(isBlank("nika"));
+        assertFalse(isBlank("  nika  "));
+    }
 
+    public static boolean containsWhitespace(final CharSequence seq) {
+        if (seq == null || seq.length() == 0) {
+            return false;
+        }
+        final int strLen = seq.length();
+        for (int i = 0; i < strLen; i++) {
+            if (Character.isWhitespace(seq.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-
-
+    @Test
+    public void testContainsWhitespace() {
+        assertFalse(containsWhitespace(null));
+        assertFalse(containsWhitespace(""));
+        assertFalse(containsWhitespace("n"));
+        assertFalse(containsWhitespace("nika"));
+        assertTrue(containsWhitespace(" "));
+        assertTrue(containsWhitespace(" n"));
+        assertTrue(containsWhitespace("nika "));
+        assertTrue(containsWhitespace("n a"));
+        assertTrue(containsWhitespace("n  a"));
     }
 }
