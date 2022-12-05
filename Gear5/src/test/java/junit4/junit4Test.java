4bbe2c305ed30a290d294fc5798b308b93ba89d6
ElijahTanimowo3560 <n01433560@humbermail.ca>
2022-12-04 19:40:43 -0500

    added user ids and names to test codes Elijah Tanimowo N01433560

--- Gear5/src/test/java/junit4/junit4Test.java
+++ Gear5/src/test/java/junit4/junit4Test.java
@@ -1,6 +1,6 @@
+// CENG-322-0NC Francisco Santos n01423860, Pradeep Singh n00975892
+// CENG-322-0NB Ralph Robes n01410324, Elijah Tanimowo n01433560
 package junit4;
-
-
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
